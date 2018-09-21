dd74dd35987ee935e0cc62aca360cdc38997b0d2
antonio miranda <antsmiranda@gmail.com>
2018-09-21 02:54:22 +0200

    Check for cordova earlier on generator

--- generator.js
+++ generator.js
@@ -5,6 +5,12 @@ const { spawnSync } = require('child_process')
 const { info } = require('@vue/cli-shared-utils')
 
 module.exports = (api, options) => {
+  // early return if cordova binary is not found
+  const hasCordova = hasbin.sync('cordova')
+  if (!hasCordova) {
+    api.exitLog(`Unable to find cordova binary, make sure it's installed.`, 'error')
+    return
+  }
   // cordova options
   const cordovaPath = options.cordovaPath || defaults.cordovaPath
   const id = options.id || defaults.id
@@ -62,12 +68,6 @@ module.exports = (api, options) => {
   })
 
   api.onCreateComplete(() => {
-    // early return if cordova binary is not found
-    const hasCordova = hasbin.sync('cordova');
-    if (!hasCordova) {
-      api.exitLog(`Unable to find cordova binary, make sure it's installed.`, 'error')
-      return
-    }
     // .gitignore - not included in files on postProcessFiles
     const ignorePath = '.gitignore'
     const ignoreCompletePath = api.resolve(ignorePath)
